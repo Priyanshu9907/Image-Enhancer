a2eef040deab9f3e1b25829fe90e88dc95f52039
Priyanshu Gupta <122674919+Priyanshu9907@users.noreply.github.com>
2023-01-21 19:37:41 +0530

    Update image_processing.py

--- image_processing.py
+++ image_processing.py
@@ -1,7 +1,7 @@
 import cv2
-img=cv2.imread('Monalisa.jpg') #write your image name or path
+img=cv2.imread('') #write your image name or path inside inverted comma
 
-#prepareatio for CLAHE
+#prepareatio for CLAHE which is useful for conversion
 clahe=cv2.createCLAHE()
 #conversion in black and white image
 gray_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -10,6 +10,3 @@ enh_img=clahe.apply(gray_img)
 #save it to a file
 cv2.imwrite('enhanced.png',enh_img)
 print('Done Enhancement')
-
-
-
